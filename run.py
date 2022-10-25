6f8acfff7c41210eddf10426bd466bc452c73c47
RKorets <93273641+RKorets@users.noreply.github.com>
2022-10-25 21:58:09 +0300

    Update run.py

--- run.py
+++ run.py
@@ -5,4 +5,4 @@ from app import create_app
 # app.run(debug=True)
 if __name__ == '__main__':
     app = create_app()
-    app.run(host='vps54480.hyperhost.name', port=80, debug=False)
+    app.run(host='vps54480.hyperhost.name', port=5000, debug=False)
